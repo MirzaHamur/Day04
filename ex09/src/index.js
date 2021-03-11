f89944f249d7fc64469bc6a67c5a0e89a1e6cdfe
Mirza <mirza.hamurko@gmail.com>
2021-03-11 17:07:34 +0100

    ex09

---
+++ ex09/src/index.js
@@ -0,0 +1,30 @@
+function main (numOne, numTwo){
+    
+     
+     // Only change code below this line 
+     var myX;
+     var myY;
+     myVariable = myVariable * 8;
+     myVariable *= 8;
+
+     
+     myVariable = myVariable / 8;
+     myVariable /= 8;
+    // Only change code below this line
+
+     return {
+         firstNum,
+         secondNum,
+         thirdNum
+     };
+    
+
+    
+     
+     }
+     
+    
+    
+    console.log(main());
+    module.exports = main;
+    
