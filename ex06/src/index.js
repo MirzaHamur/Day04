95a86c16dcbb32987676ed5d2adb4a471714e332
Mirza <mirza.hamurko@gmail.com>
2021-03-11 17:00:31 +0100

    ex06

---
+++ ex06/src/index.js
@@ -0,0 +1,18 @@
+function main (numOne, numTwo){
+    
+     
+     // Only change code below this line 
+    var x = 5.5;
+    var y = 2.5;
+    var z = x/y;
+     // Only change code below this line
+
+    
+     return myQuotient
+     }
+     
+    
+    
+    console.log(main());
+    module.exports = main;
+    
