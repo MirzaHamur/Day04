f7697005a2ecd90e56e14a2ae35532a460f729a3
Mirza <mirza.hamurko@gmail.com>
2021-03-11 13:47:04 +0100

    ex02 fix,ex03

---
+++ ex03/src/index.js
@@ -0,0 +1,14 @@
+function main (){
+var myRes = 16;
+ 
+ // Only change code below this line 
+var x = 16;
+var y = x--;
+ // Only change code below this line
+
+ return myRes;
+
+}
+
+console.log(main());
+module.exports = main;
