0215690e61b2c222b18f9f6dbcf0ecb798c6f461
Mirza <mirza.hamurko@gmail.com>
2021-03-11 20:50:39 +0100

    ex15

---
+++ ex15/src/index.js
@@ -0,0 +1,22 @@
+function main (){
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <p>Ovo je paragraph</p>
+    
+    </head>
+    <body>
+   <a href ="https://www.pexels.com/search/cat/" target="_blank">cat photos</a>
+    
+    </body>
+    
+    </html>
+   
+
+
+     
+    
+    
+    console.log(main());
+    module.exports = main;
+    
