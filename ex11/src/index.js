45fbcafcf7c1c170015bb007a2211652fef532f9
Mirza <mirza.hamurko@gmail.com>
2021-03-11 20:42:09 +0100

    ex13

---
+++ ex11/src/index.js
@@ -0,0 +1,23 @@
+function main (){
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <h1>Naslov 1</h1>
+    <p>Ovo je paragraph</p>
+    <h2>Ovo je 2 naslov</h2>
+    </head>
+    
+    <body>
+    The content of the document......
+    </body>
+    
+    </html>
+   
+
+
+     
+    
+    
+    console.log(main());
+    module.exports = main;
+    
---
+++ ex13/src/index.js
@@ -0,0 +1,26 @@
+function main (){
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <main>
+    <p>Hello Paragraph</p>
+    <h2>Ovo je 2 naslov</h2>
+    <p>Purr jump eat the grass rip the couch scratched sunbathe,shed everywhere rip the couch sleep in the sin fluffy fur catnip scratched</p>
+    </head>
+    </main>
+    </head>
+    
+    <body>
+    The content of the document......
+    </body>
+    
+    </html>
+   
+
+
+     
+    
+    
+    console.log(main());
+    module.exports = main;
+    
