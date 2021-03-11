0126f69923ec8f91860a551851fb4427e98829a8
Mirza <mirza.hamurko@gmail.com>
2021-03-11 17:09:37 +0100

    ex10

---
+++ ex10/src/index.js
@@ -0,0 +1,21 @@
+function main (numOne, numTwo){
+    
+     
+     // Only change code below this line 
+     var myHomeTown ="Sarajevo";
+    // Only change code below this line
+    return {
+    myFirstName,
+
+    myLaseName,
+
+    myHomeTown
+    };
+
+}
+     
+    
+    
+    console.log(main());
+    module.exports = main;
+    
