b8f100062b28a8d25550d3f0baaa1d63ec81b89d
Mirza <mirza.hamurko@gmail.com>
2021-03-11 14:03:30 +0100

    ex04

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,16 +1,17 @@
 function main (){
-var myRes = 16;
- 
- // Only change code below this line 
-var myDecimal = 8.2;
-var myDecimalE = 0.0254;
- // Only change code below this line
-return {
- myDecimal,
- myDecimalE
- };
- 
-}
-
-console.log(main());
-module.exports = main;
+    
+     
+     // Only change code below this line 
+    var myDecimal = 8.2;
+    var myDecimalE = 0.0254;
+     // Only change code below this line
+    return {
+     myDecimal,
+     myDecimalE
+     };
+     
+    }
+    
+    console.log(main());
+    module.exports = main;
+    
