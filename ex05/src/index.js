b1b98240b8b543c2ab0bcfa9b1c67c57d5569187
Mirza <mirza.hamurko@gmail.com>
2021-03-11 14:02:26 +0100

    ex05

---
+++ ex05/src/index.js
@@ -0,0 +1,15 @@
+function main (){
+
+ 
+ // Only change code below this line 
+   var product = 2.5 * 0.0 <<float ; 
+   var x = 2.5;
+   var y = 2.0;
+   var z = x*y;
+ // Only change code below this line
+return product;
+ 
+}
+
+console.log(main(numOne, numTwo));
+module.exports = main;
