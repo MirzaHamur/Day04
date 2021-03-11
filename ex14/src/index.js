d9b198673d719733bb00217ecbccc573eaec19a3
Mirza <mirza.hamurko@gmail.com>
2021-03-11 20:49:21 +0100

    ex14

---
+++ ex14/src/index.js
@@ -0,0 +1,23 @@
+function main (){
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <p>Ovo je paragraph</p>
+    
+    </head>
+    <body>
+<img src= "https://www.pexels.com/photo/gray-cat-on-tree-branch-1693441"/
+  alt ="Author standing on a beach with two thumbs up.">
+    
+    </body>
+    
+    </html>
+   
+
+
+     
+    
+    
+    console.log(main());
+    module.exports = main;
+    
