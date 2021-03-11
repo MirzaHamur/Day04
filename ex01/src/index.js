e2cf718fcb6bc9f9b2879749af6b4100f05c2aa4
Mirza <mirza.hamurko@gmail.com>
2021-03-11 13:39:40 +0100

    ex01

---
+++ ex01/src/index.js
@@ -0,0 +1,28 @@
+function main (numOne, numTwo){
+
+var quotient;
+if(numTwo === 0) {
+    quotient =  'Divide by 0! Cannot divide by zero!';
+
+} else {
+   // Only change code below this line
+    var x = 25;
+    var y = 5;
+    var z = x/y;
+
+    var a = 420;
+    var b = 10;
+    var c = a/b;
+
+    var d = 55;
+    var e = 0;
+    var f = d/0;
+  //  Only change code below this line
+}
+
+ return quotient;
+
+}
+
+console.log(main(numOne, numTwo));
+module.exports = main; //change this line
