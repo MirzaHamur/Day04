f40552f818608802a860207e4027a1e05eb7bc61
Mirza <mirza.hamurko@gmail.com>
2021-03-11 17:05:36 +0100

    ex08

---
+++ ex08/src/index.js
@@ -0,0 +1,27 @@
+function main (numOne, numTwo){
+    
+     
+     // Only change code below this line 
+     var myX;
+     var myY;
+     x = 12;
+     y = 19;
+     t = 24;
+     z = x+=t;
+     myVariable = myVariable + 8;
+     myVariable +8= 8;
+     
+     myVariable = myVariable -8;
+     myVariable -= 8;
+    
+     // Only change code below this line
+
+    
+     
+     }
+     
+    
+    
+    console.log(main());
+    module.exports = main;
+    
