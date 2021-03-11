c5315d578773c5a35572eef37814023e0e76e175
Mirza <mirza.hamurko@gmail.com>
2021-03-11 17:02:41 +0100

    ex07

---
+++ ex07/src/index.js
@@ -0,0 +1,22 @@
+function main (numOne, numTwo){
+    
+     
+     // Only change code below this line 
+     
+     7 % 2 = 1 because;
+     Math.floor(7 / 2) = 3;
+     3 * 2 = 6;
+     7 - 6 = 1;
+     19 % 2 = 1;
+     52 % 2 = 0;
+     // Only change code below this line
+
+    
+     return myQuotient
+     }
+     
+    
+    
+    console.log(main());
+    module.exports = main;
+    
